d3ac935a5f7c557133a666e9b0ceec04c7bf0cfb
developer110_Atiq <developer109.fzeetechz@gmail.com>
2024-02-20 11:56:50 +0530

    --commit new

--- src/Layout/Drawer/DrawerJs.jsx
+++ src/Layout/Drawer/DrawerJs.jsx
@@ -12,6 +12,7 @@ import {
   } from '@chakra-ui/react'
 import React from "react"
 import { TiThMenu } from "react-icons/ti";
+import { NavLink } from "react-router-dom";
 
 export function DrawerJs({ isOpen, onOpen, onClose,btnRef }) {
   
@@ -22,23 +23,43 @@ export function DrawerJs({ isOpen, onOpen, onClose,btnRef }) {
         <Drawer
           isOpen={isOpen}
           placement='right'
+          key={"xs"}
           onClose={onClose}
           finalFocusRef={btnRef}
+          h={50}
+          w={20}
         >
           <DrawerOverlay />
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerHeader>Create your account</DrawerHeader>
+            <DrawerHeader>
+            <img src="https://seeklogo.com/images/E/education-college-school-logo-289049824A-seeklogo.com.png" className="h-8" alt="Flowbite Logo" />
+            </DrawerHeader>
   
-            <DrawerBody>
-              <Input placeholder='Type here...' />
-            </DrawerBody>
+            <ul className="flex responsiveul flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+          <NavLink to='' className=" nav-link block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to='' className=" nav-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</NavLink>
+        </li>
+        <li>
+          <NavLink to='' className=" nav-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Courses</NavLink>
+        </li>
+        <li>
+          <NavLink to='' className=" nav-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pages</NavLink>
+        </li>
+        <li>
+          <NavLink to='' className=" nav-link block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</NavLink>
+        </li>
+       
+      </ul>
   
             <DrawerFooter>
-              <Button variant='outline' mr={3} onClick={onClose}>
+              {/* <Button variant='outline' mr={3} onClick={onClose}>
                 Cancel
-              </Button>
-              <Button colorScheme='blue'>Save</Button>
+              </Button> */}
+              {/* <Button colorScheme='blue'>Save</Button> */}
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
